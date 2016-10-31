c4d165966276b6c27ab25633522051f18569dd62
gurguk <you@example.com>
2016-10-31 07:45:46 +0200

    DZ-3: Create Abstract class, add Inherite.

---
+++ source/Log/Log.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Roman\Parser\Log;
+
+
+abstract class Log
+{
+    public function writeLog($logfile, $text)
+    {
+        $fp = fopen($logfile,'rw');
+        fwrite($fp,$text.PHP_EOL);
+        fclose($fp);
+    }
+}

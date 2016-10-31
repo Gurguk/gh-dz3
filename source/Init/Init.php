16c24e344578454b7dfad774b7703593af4e6976
gurguk <you@example.com>
2016-10-31 07:43:28 +0200

    DZ-3: Create Interface. Init part.

---
+++ source/Init/Init.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Roman\Parser\Init;
+
+interface Init
+{
+    /*
+     * @notice create folder for xml file
+     * */
+    public function makeUploadFolder();
+}
---
+++ source/Init/InitClass.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Roman\Parser\Init;
+
+
+class InitClass implements Init
+{
+    /*
+    * @var path to folder
+    * */
+    private $folderpath;
+
+    public function __construct($folderpath)
+    {
+        $this->folderpath = $folderpath;
+    }
+    /*
+     * @notice create folder for xml file
+     * @param string $folderpath
+     * */
+    public function makeUploadFolder()
+    {
+        $result = FALSE;
+        if(!is_dir($this->folderpath)){
+            $result = mkdir($this->folderpath);
+        }
+        return $result;
+    }
+}

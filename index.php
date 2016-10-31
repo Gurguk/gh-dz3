92cf89ec99ea2bf0e571176c7af08dde5fe51303
gurguk <you@example.com>
2016-10-31 07:48:09 +0200

    DZ-3: Create Trait.

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Roman\Parser\Parser;
 
-$class=new Parser('/var/www/dz-3/upload/','/var/www/dz-3/log.log');
+$class=new Parser('/var/www/dz-no3/upload/','/var/www/dz-no3/log.log');
 $class->parseXML();
---
+++ source/ExampleTrait.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roman
+ * Date: 31.10.16
+ * Time: 1:15
+ */
+
+namespace Roman\Parser;
+
+
+trait ExampleTrait
+{
+    private function uploadFile()
+    {
+        move_uploaded_file($_FILES['userfile']['tmp_name'],$this->folderpath.'/xml.xml');
+    }
+}
